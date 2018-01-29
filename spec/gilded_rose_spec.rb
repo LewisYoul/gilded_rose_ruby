b70f3d9de53c604c9b4a36775c77810abf5525fa
Lewis <lewisyoul@gmail.com>
2018-01-29 14:15:04 +0000

    Add tests for 'Aged Brie' and 'Sulfuras'

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,7 @@ describe GildedRose do
       Item.new(name="+5 Dexterity Vest", sell_in=10, quality=0),
       Item.new(name="+5 Dexterity Vest", sell_in=-1, quality=10),
       Item.new(name="Aged Brie", sell_in=2, quality=0),
+      Item.new(name="Aged Brie", sell_in=2, quality=50),
       Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
       Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
       Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
@@ -46,6 +47,25 @@ describe GildedRose do
       it "reduces sell_in by 1 when a day passes" do
         @shop.update_quality()
         expect(@shop.items[3].sell_in).to eq(1)
+      end
+      it "increases the quality by 1 when a day passes" do
+        @shop.update_quality()
+        expect(@shop.items[3].quality).to eq(1)
+      end
+      it "doesn't increase the quality by 1 if already at 50" do
+        @shop.update_quality()
+        expect(@shop.items[4].quality).to eq(50)
+      end
+    end
+
+    describe "Sulfuras" do
+      it "doesn't decrease the sell_in by 1" do
+        @shop.update_quality()
+        expect(@shop.items[6].sell_in).to eq(0)
+      end
+      it "doesn't increase the quality by 1" do
+        @shop.update_quality()
+        expect(@shop.items[6].quality).to eq(80)
       end
     end
   end
