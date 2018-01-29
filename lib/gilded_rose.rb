9a984cc6cc34dc82728b4dff5ade1c5453d7661e
Lewis <lewisyoul@gmail.com>
2018-01-29 15:59:13 +0000

    Extract to private methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -13,22 +13,41 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      reduce_sell_in(item)
       if (!@unique.key(item.name))
-        item.sell_in -= 1
-        item.quality -= 1 if item.quality > 0
-        item.quality -= 1 if item.sell_in < 0
+        update_generic(item)
       elsif item.name == @unique[:brie]
-        item.sell_in -= 1
-        item.quality += 1 if item.quality < 50
+        increase_quality(item) if item.quality < 50
       elsif item.name == @unique[:pass]
-        item.sell_in -= 1
-        item.quality += 1
-        item.quality += 1 if item.sell_in <= 10 && item.quality < 50
-        item.quality += 1 if item.sell_in <= 5 && item.quality < 50
+        update_pass(item)
       end
     end
   end
 
+  private
+
+  def update_pass(item)
+    increase_quality(item)
+    increase_quality(item) if item.sell_in <= 10 && item.quality < 50
+    increase_quality(item) if item.sell_in <= 5 && item.quality < 50
+  end
+
+  def update_generic(item)
+    decrease_quality(item) if item.quality > 0
+    decrease_quality(item) if item.sell_in < 0
+  end
+
+  def reduce_sell_in(item)
+    item.sell_in -= 1 unless item.name == @unique[:sulfuras]
+  end
+
+  def increase_quality(item)
+    item.quality += 1
+  end
+
+  def decrease_quality(item)
+    item.quality -= 1
+  end
 
 end
 
